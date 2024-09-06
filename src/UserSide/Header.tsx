9bc00e0465d06e8e8e347857387322420f59361f
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-09-06 21:29:31 +0530

    added header component and its funcitons

--- src/UserSide/Header.tsx
+++ src/UserSide/Header.tsx
@@ -1,11 +1,19 @@
 
+import { useState } from 'react';
 import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai';
 
 function Header() {
+
+  const [nav,setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
   return (
     <>
        <div style={{backgroundColor:'#078FDC'}} className='text-white font-bold h-[130px] flex justify-between'  >
-            <h1 className="w-full text-3xl text-black font-exo  p-10" >FREEZE <span className="text-white font-exo" >LAND</span> </h1>
+            <h1 className="w-full text-3xl  text-black font-exo  p-10" >FREEZE <span className="text-white font-exo" >LAND</span> </h1>
             <ul className="flex p-8  ">
                 <li className="p-4">HOME</li>
                 <li className="p-4">ACCOUNT</li>
@@ -13,16 +21,18 @@ function Header() {
                 <li className="p-4">CONTACT</li>
                 <li className="p-4">QUEUE</li>
             </ul>
-            <div className='p-11' >
-                <AiOutlineMenu size={25}/>
+            <div className='p-11' onClick={handleNav} >
+              {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={25}/>}
+                
             </div>
-            <div className='fixed left-0 top-0 w-[60%] border-r h-full border-r-gray-900' >
-                <ul className='text-black pt-24'>
-                    <li className="p-4">HOME</li>
-                    <li className="p-4">ACCOUNT</li>
-                    <li className="p-4">SERVICES</li>
-                    <li className="p-4">CONTACT</li>
-                    <li className="p-4">QUEUE</li>
+            <div className='fixed left-0 top-0 w-[60%] border-r h-full border-r-gray-900 bg-[#078FDC]' >
+            <h1 className="w-full text-3xl text-black font-exo  p-10" >FREEZE <span className="text-white font-exo" >LAND</span> </h1>
+                <ul className='text-black pl-6'>
+                    <li className="p-4 border-b border-white-600">HOME</li>
+                    <li className="p-4 border-b">ACCOUNT</li>
+                    <li className="p-4 border-b">SERVICES</li>
+                    <li className="p-4 border-b">CONTACT</li>
+                    <li className="p-4 border-b">QUEUE</li>
                 </ul>
             </div>
        </div>
