303ce8b53d4040250b91dfc1b7afa7b636738c63
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-09-20 12:16:03 +0530

    added routes for HomePage and singup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import './App.css'
-import SignupPage from './Pages/UserSignupPage'
-
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import UserHomePage from "./Pages/UserHomePage";
+import UserSignupPage from "./Pages/UserSignupPage";
 
 function App() {
-
-
   return (
     <>
-    {/* <SignupPage/> */}
-      {/* <LoginPage/> */}
-    
-      
+      <Routes>
+        {/* userRoutes */}
+        <Route path="homepage" element={<UserHomePage />} />
+        <Route path="SignUp" element={<UserSignupPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
