b04b8a57671564ff2b59cafccfe63a88a814a7a8
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-10-13 21:31:50 +0530

    added some changes in admin side for responsiveness

--- src/Pages/Admin/AdminDashboard.tsx
+++ src/Pages/Admin/AdminDashboard.tsx
@@ -1,17 +1,20 @@
+import { Outlet } from "react-router-dom";
 import AdminHeader from "../../components/Admin/AdminHeader";
 import AdminListing from "../../components/Admin/AdminListing";
 const AdminDashboard: React.FC = () => {
   return (
     <>
-      <div className="h-screen flex">
+      <div className="flex">
         {/* side bar */}
         <AdminListing />
+        {/* side bar */}
+
         {/* Contents */}
-        <div className="w-3/4 bg-[#DFECF8]">
-          <div>
-            <AdminHeader heading="Dashboard" />
-          </div>
+        <div className="w-full ml-16 md:ml-56 bg-[#DFECF8]">
+          <AdminHeader heading="Dashboard" />
+          <Outlet />
         </div>
+        {/* Contents */}
       </div>
     </>
   );
--- src/components/Admin/AdminListing.tsx
+++ src/components/Admin/AdminListing.tsx
@@ -6,18 +6,22 @@ import toast from "react-hot-toast";
 import { adminLogout } from "../../Api/admin";
 import { adLogout } from "../../App/slices/AuthSlice";
 import { useNavigate } from "react-router-dom";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { FaUsers } from "react-icons/fa6";
+import { GiMechaHead } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 type MenuItem = {
   id: number;
   name: string;
   icon: string;
-  path:string;
+  path: string;
 };
 
 const AdminListing: React.FC = () => {
   const navigate = useNavigate();
   const [activeItem, setActiveItem] = useState<number>(0);
-  const handleItemClick = (item:MenuItem) => {
+  const handleItemClick = (item: MenuItem) => {
     setActiveItem(item.id);
     navigate(item.path);
   };
@@ -51,59 +55,68 @@ const AdminListing: React.FC = () => {
     }
   };
 
+  const SIDEBAR_LINKS = [
+    {
+      name: "Dashboard",
+      icon: MdOutlineSpaceDashboard,
+      id: 1,
+      path: "/admin/dashboard",
+    },
+    { name: "Users", icon: FaUsers, id: 2, path: "/admin/users" },
+    { name: "Mechanics", icon: GiMechaHead, id: 3, path: "/admin/mech" },
+  ];
+
   return (
     <>
       {/* Side Bar */}
-      <div className="w-1/4 bg-[#4B4B4B]">
+      <div className="w-16 md:w-56 fixed text-white left-0 top-0 z-10 h-screen border-r bg-[#4B4B4B]">
         {/* Heading */}
         <div className="text-center">
-          <h1 className="w-full text-2xl text-freeze-color font-exo p-10">
+          <h1 className="w-full text-xl md:text-2xl text-freeze-color font-exo p-6 md:p-10">
             FREEZE <span className="text-white font-exo">LAND</span>
           </h1>
         </div>
 
         {/* Profile */}
-        <div className="rounded-full overflow-hidden flex justify-center items-center">
-          <img
-            src="/src/Images/businessman.png"
-            alt="User profile picture"
-            className="object-cover w-16 h-16"
-          />
+        <div className="flex justify-center items-center mt-4 md:mt-8">
+          <div className="rounded-full overflow-hidden w-10 h-10 md:w-16 md:h-16">
+            <img
+              src="/src/Images/businessman.png"
+              alt="User profile picture"
+              className="object-cover w-full h-full"
+            />
+          </div>
         </div>
-        <h2 className="text-white text-center">Sandeep S</h2>
+        <h2 className="text-white text-center text-sm md:text-base mt-2 md:mt-4">
+          Sandeep S
+        </h2>
 
         {/* Listing */}
-        <div className="mx-12 mt-8 space-y-5">
-          {[
-            { name: "Dashboard", icon: "/src/Images/dashboard.png",id:1,path:"/admin/dashboard" },
-            { name: "Users", icon: "/src/Images/teamwork.png",id:2,path:"/admin/users" },
-            { name: "Mechanics", icon: "/src/Images/support.png",id:3,path:"/admin/mech" },
-          ].map((item) => (
-            <div
-              key={item.name}
-              className={`flex items-center space-x-3 p-2 rounded-lg cursor-pointer group 
-              ${
-                activeItem === item.id ? "bg-gray-600" : ""
-              } hover:bg-gray-500`}
-              onClick={() => handleItemClick(item)}
-            >
-              <img
-                src={item.icon}
-                alt={`${item.name} Icon`}
-                className="w-5 h-5 group-hover:scale-110 transition-transform"
-              />
-              <h4 className="text-white group-hover:text-gray-300">
-                {item.name}
-              </h4>
-              <RiArrowRightDoubleFill className="text-white group-hover:text-gray-300" />
-            </div>
-          ))}
+        <div className="mx-4 md:mx-12 mt-6 space-y-4 md:space-y-5">
+          <ul className="space-y-4 md:space-y-6">
+            {SIDEBAR_LINKS.map((link, index) => (
+              <li
+                key={index}
+                className="font-medium rounded-md py-2 hover:bg-freeze-color transition-all duration-300"
+              >
+                <Link
+                  to={link.path}
+                  className="flex items-center justify-center md:justify-start md:space-x-5"
+                >
+                  <span>{link.icon({})}</span>
+                  <span className="hidden md:inline text-sm md:text-base">
+                    {link.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
 
         {/* Logout Button */}
-        <div className="flex justify-center items-center mt-12">
+        <div className="flex justify-center items-center mt-10 md:mt-12">
           <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 md:py-2 md:px-4 rounded transition-all duration-300"
             onClick={handleLogout}
           >
             Logout
