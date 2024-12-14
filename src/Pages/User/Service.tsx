932018d9f689dd869bf38322daef2189cb5f3972
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-12-14 19:52:05 +0530

    added a option for redio button

--- src/Pages/User/Service.tsx
+++ src/Pages/User/Service.tsx
@@ -6,9 +6,10 @@ import { FiMapPin } from "react-icons/fi";
 import { getProfile } from "../../Api/user";
 import { Card, CardContent, Typography } from "@mui/material";
 import { AddAddress } from "../../interfaces/AddAddress";
+import Footer from "../../components/User/Footer";
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
-interface ServiceData{
+interface ServiceData {
   _id: string;
   name: string;
   discription: string;
@@ -32,18 +33,15 @@ const Service: React.FC = () => {
           getService(id),
           getProfile(),
         ]);
-        if (serviceResult){
-          console.log(
-            "Service result from the backend",
-            serviceResult.data
-          );
+        if (serviceResult) {
+          console.log("Service result from the backend", serviceResult.data);
           setServices(serviceResult.data);
         }
 
         if (profileResult) {
           const profileData = profileResult.data.data.data;
           setUserProfile(profileResult.data.data.data);
-          console.log("Profile reuslt from the backend",userProfile);
+          console.log("Profile reuslt from the backend", userProfile);
           const defaultAdd = profileData.address.find(
             (addr: AddAddress) => addr._id == profileData.defaultAddress
           );
@@ -65,10 +63,8 @@ const Service: React.FC = () => {
   // Handle location fetching
   const handleFetchLocation = () => {
     navigator.geolocation.getCurrentPosition(
-      
       async (position) => {
         const { latitude, longitude } = position.coords;
-      
 
         try {
           const response = await fetch(
@@ -103,15 +99,15 @@ const Service: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <Header />
-      <div className="flex justify-between mt-40 mx-12 ">
-        <div>
-          <div>
+      <div className="md:flex justify-evenly mt-20 mx-6 ">
+        <div className="">
+          <div className="mb-12 flex justify-center">
             <h1 className=" font-Metal text-xl">{service?.discription}</h1>
           </div>
-          <div>
-              <img src={service?.image} alt="" />
+          <div className="mb-12">
+            <img src={service?.image} alt="" />
           </div>
         </div>
         <div>
@@ -122,7 +118,7 @@ const Service: React.FC = () => {
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-first-name"
                 >
-                  Name
+                  Name *
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -155,7 +151,7 @@ const Service: React.FC = () => {
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-complaint"
                 >
-                  Complaint Description
+                  Complaint Description *
                 </label>
                 <textarea
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -168,6 +164,83 @@ const Service: React.FC = () => {
                 </p>
               </div>
             </div>
+
+            {/**providing space for inserting the device image*/}
+            <label
+              htmlFor=""
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+              Upload Image *
+            </label>
+            <div className="flex items-center justify-center w-full mb-5">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full  border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
+            {/**providing space for inserting the device image*/}
+
+            {/**Adding address with new feature, implimenting radio button  */}
+
+            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input
+                id="bordered-radio-1"
+                type="radio"
+                value=""
+                name="bordered-radio"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor="bordered-radio-1"
+                className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Default radio
+              </label>
+            </div>
+            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input
+                checked
+                id="bordered-radio-2"
+                type="radio"
+                value=""
+                name="bordered-radio"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor="bordered-radio-2"
+                className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Checked state
+              </label>
+            </div>
+
+            {/**Adding address with new feature, implimenting radio button  */}
 
             {/**provide the default address  */}
             <label
@@ -273,6 +346,7 @@ const Service: React.FC = () => {
           </form>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
