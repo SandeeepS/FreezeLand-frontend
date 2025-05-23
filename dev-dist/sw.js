45dbbeabe05def09580fe57130d44399eb05d2c4
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-05-23 21:39:19 +0530

    merged the week-4 branch 2nd time

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m8koujkd2f8"
+    "revision": "0.j2ll51dg1t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
