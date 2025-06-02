c0ee28c8ced76496c769eb66ce9d4179c9197b13
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-02 13:58:54 +0530

    changed made in the dev-dist/sw.js file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mvt6nhak76o"
+    "revision": "0.7dlq9p3g7ng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
