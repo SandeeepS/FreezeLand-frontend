c44d213fdf8247831bd63d786aee451fd6a1ab19
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-13 15:08:16 +0530

    added the freezeland.online in the baseURL in the axios.ts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e0kv6gu9vi8"
+    "revision": "0.0vf2qrkkv3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "http://freezeland.online/api",
   withCredentials: true,
 });
 
