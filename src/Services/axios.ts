c8ae59cd6fb1e25ebc0b68c84e99f1997a8e2f54
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-14 10:29:32 +0530

    moved the images to the public folder

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://freezeland.space/api",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://freezeland.space/api",
   withCredentials: true,
 });
 
