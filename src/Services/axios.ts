99670f3addfc9e5ef21e11e757ef55d77c74f37d
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-06-27 13:57:06 +0530

    modified the axios.ts

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 
 //use this isUsingProduction false for locally testing 
-const isUsingProduction = true; 
+const isUsingProduction = true;
 
 const Api: AxiosInstance = axios.create({
   baseURL: isUsingProduction
