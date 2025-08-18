32b61dd54c10d89a288cb65b83b424526c5b79c2
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-08-18 21:54:09 +0530

    craeated a Profile page as a common component

---
+++ src/components/Common/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>This is Common profile section </h1>
+      {/**make this component for both user, admin , mechanic and additon
+       *  information should be added like a extrea component for each module */}
+    </div>
+  );
+};
+
+export default Profile;
