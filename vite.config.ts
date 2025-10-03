3eb24b5fc81e245fa570794b8cd71343f328da6e
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-10-03 14:20:48 +0530

    added max size to 10 mb for the workBox

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import {VitePWA} from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
-        name: 'Freezeland',
-        short_name: 'Freezeland',
-        description: 'Your Freezeland app description',
-        theme_color: '#3367D6',
+        name: "Freezeland",
+        short_name: "Freezeland",
+        description: "Your Freezeland app description",
+        theme_color: "#3367D6",
         icons: [
           {
-            src: 'icons/penguine-192X192',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "icons/penguine-192X192",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'icons/penguine-512X512',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            src: "icons/penguine-512X512",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
       devOptions: {
-        enabled: true
-      }
-    })
+        enabled: true,
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
+      },
+    }),
   ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
-})
+  },
+});
