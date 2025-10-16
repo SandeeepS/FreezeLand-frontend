fb8fe5287d170e72e1c9646d859edaee168278d6
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-10-16 19:33:08 +0530

    added the firebase api key to the cicd

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eh5asqdlh7o"
+    "revision": "0.b2bdsvmf5k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
