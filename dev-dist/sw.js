8a07e6bd180c0e05070929f81ae5bb756be4ae30
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-10-20 15:42:56 +0530

    some changes occured in the workflows

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b2bdsvmf5k"
+    "revision": "0.p2as2ove4dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
