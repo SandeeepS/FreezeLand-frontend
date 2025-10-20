632caea3264acee5d3e76feafd9f5f073427efdb
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-10-20 15:47:34 +0530

    changed the baseurl from the .env

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -3,8 +3,7 @@ import axios, { AxiosInstance } from "axios";
 //here is the API
 const Api: AxiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL_PROD ||
-    import.meta.env.VITE_API_BASE_URL_DEV,
+    import.meta.env.VITE_API_BASE_URL_PROD,
   withCredentials: true,
 });
 
